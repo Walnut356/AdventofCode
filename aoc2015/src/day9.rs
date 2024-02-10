eebc5de34471dabce40cf44cb54e201b988d2632
Walnut <39544927+Walnut356@users.noreply.github.com>
2024-02-09 20:36:53 -0600

    day 9

---
+++ aoc2015/src/day9.rs
@@ -0,0 +1,91 @@
+#![allow(unused_imports)]
+
+use std::hint::black_box;
+
+use crate::get_data;
+use fxhash::{FxHashMap, FxHashSet};
+use itertools::{Itertools, Permutations};
+
+// sanity
+type Map<'a> = FxHashMap<&'a str, FxHashMap<&'a str, usize>>;
+type Set<'a> = FxHashSet<&'a str>;
+type Cache<'a> = FxHashMap<(&'a str, Set<'a>, usize), usize>;
+
+const NODE_COUNT: usize = 8;
+
+pub fn p1(data: String) -> usize {
+    let mut map: Map = FxHashMap::default();
+
+    for line in data.lines() {
+        let mut tokens = line.split_ascii_whitespace();
+
+        let node_1 = tokens.next().unwrap();
+        tokens.next();
+        let node_2 = tokens.next().unwrap();
+        tokens.next();
+        let dist = tokens.next().unwrap().parse::<usize>().unwrap();
+
+        if !map.contains_key(node_1) {
+            map.insert(node_1, FxHashMap::default());
+        }
+        if !map.contains_key(node_2) {
+            map.insert(node_2, FxHashMap::default());
+        }
+
+        map.get_mut(node_1).unwrap().insert(node_2, dist);
+        map.get_mut(node_2).unwrap().insert(node_1, dist);
+    }
+
+    let mut result = usize::MAX;
+    for t in map.keys().permutations(map.keys().len()) {
+        let mut dist = 0;
+        for i in 0..t.len() - 1 {
+            dist += map.get(t[i]).unwrap().get(t[i + 1]).unwrap();
+        }
+        result = result.min(dist);
+    }
+
+    result
+}
+
+pub fn p2(data: String) -> usize {
+        let mut map: Map = FxHashMap::default();
+
+    for line in data.lines() {
+        let mut tokens = line.split_ascii_whitespace();
+
+        let node_1 = tokens.next().unwrap();
+        tokens.next();
+        let node_2 = tokens.next().unwrap();
+        tokens.next();
+        let dist = tokens.next().unwrap().parse::<usize>().unwrap();
+
+        if !map.contains_key(node_1) {
+            map.insert(node_1, FxHashMap::default());
+        }
+        if !map.contains_key(node_2) {
+            map.insert(node_2, FxHashMap::default());
+        }
+
+        map.get_mut(node_1).unwrap().insert(node_2, dist);
+        map.get_mut(node_2).unwrap().insert(node_1, dist);
+    }
+
+    let mut result = 0;
+    for t in map.keys().permutations(map.keys().len()) {
+        let mut dist = 0;
+        for i in 0..t.len() - 1 {
+            dist += map.get(t[i]).unwrap().get(t[i + 1]).unwrap();
+        }
+        result = result.max(dist);
+    }
+
+    result
+}
+
+#[test]
+fn test_d9() {
+    let data = get_data(9);
+    assert_eq!(p1(data.clone()), 207);
+    assert_eq!(p2(data), 804);
+}
--- aoc2015/src/lib.rs
+++ aoc2015/src/lib.rs
@@ -18,7 +18,7 @@ pub mod day13;
 pub mod day12;
 pub mod day11;
 pub mod day10;
-pub mod dayj;
+pub mod day9;
 pub mod day8;
 pub mod day7;
 pub mod day6;
