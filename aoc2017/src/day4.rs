ac535554867e2c5e69241ec1ec6c3c6364c63ace
Walnut <39544927+Walnut356@users.noreply.github.com>
2024-04-12 20:36:13 -0500

    day 4

--- aoc2017/src/day4.rs
+++ aoc2017/src/day4.rs
@@ -1,22 +1,55 @@
 #![allow(unused_imports)]
 
+use itertools::Itertools;
+
 use crate::get_data;
+use crate::utils::Set;
 
 pub fn p1(data: String) -> usize {
-    let result = 0;
+    let mut result = 0;
+    let mut set: Set<&str> = Set::default();
 
-    return result;
+    data.lines().for_each(|line| {
+        let mut r = 1;
+        for t in line.split_ascii_whitespace() {
+            if !set.insert(t) {
+                r = 0;
+                break;
+            };
+        }
+        set.clear();
+
+        result += r;
+    });
+
+    result
 }
 
 pub fn p2(data: String) -> usize {
-    let result = 0;
+    let mut result = 0;
+    let mut set: Set<Vec<u8>> = Set::default();
 
-    return result;
+    data.lines().for_each(|line| {
+        let mut r = 1;
+        for t in line.split_ascii_whitespace() {
+            if !set.insert(t.as_bytes().to_owned()) {
+                r = 0;
+            }
+            for p in t.bytes().permutations(t.len()) {
+                set.insert(p);
+            }
+        }
+        set.clear();
+
+        result += r;
+    });
+
+    result
 }
 
 #[test]
 fn test_d4() {
     let data = get_data(4);
-    assert_eq!(p1(data.clone()), 0);
-    assert_eq!(p2(data), 0);
+    assert_eq!(p1(data.clone()), 325);
+    assert_eq!(p2(data), 119);
 }
--- aoc2017/src/main.rs
+++ aoc2017/src/main.rs
@@ -2,7 +2,7 @@ use std::time::Instant;
 
 use aoc2017::*;
 
-day! {3}
+day! {4}
 
 fn main() {
     let data = get_data(DAY);
