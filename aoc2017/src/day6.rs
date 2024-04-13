7011ce5751e8bf28cb1d8b14a1b62b9e1bc56aa8
Walnut <39544927+Walnut356@users.noreply.github.com>
2024-04-12 21:23:13 -0500

    day 6

--- aoc2017/src/day6.rs
+++ aoc2017/src/day6.rs
@@ -1,17 +1,68 @@
 #![allow(unused_imports)]
 
+use itertools::Itertools;
+
 use crate::get_data;
+use crate::utils::{Set, Map};
 
 pub fn p1(data: String) -> usize {
-    let result = 0;
+    let mut mem = data
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+    let mut states: Set<Vec<u32>> = Set::default();
+    states.insert(mem.clone());
 
-    return result;
+    let mut steps = 1;
+    loop {
+        let max_val = *mem.iter().max().unwrap();
+        let dist = mem.iter().position(|&x| x == max_val).unwrap();
+        mem[dist] = 0;
+        let mut i = dist;
+        for _ in 0..max_val {
+            i += 1;
+            if i >= mem.len() {
+                i = 0;
+            }
+            mem[i] += 1;
+        }
+
+        // dbg!(&mem);
+        if !states.insert(mem.clone()) {
+            return steps;
+        }
+        steps += 1;
+    }
 }
 
 pub fn p2(data: String) -> usize {
-    let result = 0;
+    let mut mem:[u32; 16] = data
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<_>>().try_into().unwrap();
+    let mut states: Map<[u32; 16], usize> = Map::default();
+    states.insert(mem, 0);
 
-    return result;
+    let mut steps = 1;
+    loop {
+        let max_val = *mem.iter().max().unwrap();
+        let dist = mem.iter().position(|&x| x == max_val).unwrap();
+        mem[dist] = 0;
+        let mut i = dist;
+        for _ in 0..max_val {
+            i += 1;
+            if i >= mem.len() {
+                i = 0;
+            }
+            mem[i] += 1;
+        }
+
+        // dbg!(&mem);
+        if let Some(v) = states.insert(mem, steps) {
+            return steps - v;
+        }
+        steps += 1;
+    }
 }
 
 #[test]
